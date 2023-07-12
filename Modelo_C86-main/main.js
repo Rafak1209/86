660f4181c136380d27a8304fdc68fc0c32254302
Rafak1209 <126103840+Rafak1209@users.noreply.github.com>
2023-07-12 13:55:28 -0300

    Add files via upload

---
+++ Modelo_C86-main/main.js
@@ -0,0 +1,31 @@
+var canvas = new fabric.Canvas('myCanvas');
+ var x= document.getElementById("myAudio");
+
+function newImage()
+{
+	
+    fabric.image,fromURL('birthdayImage.jpg', functio(img) {
+        blockImageObject = Img;
+        blockImageObject.scaleToWidht(700);
+        blockImageObject.scaleToHeigth(510);
+        blockImageObject.set({
+        top:0,
+        left:0
+        });
+        canvas.add(blockimageObject);
+    });
+	
+}
+
+function playSound(){
+    x.play();
+	
+}
+
+#myCanvas
+{
+    border-width: 10px;
+    background-color: white;
+    border-style:ridge;
+    border-color:darkviolet;
+}
